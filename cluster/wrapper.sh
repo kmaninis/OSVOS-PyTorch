b0dfac3e699a37a2584e8c9cea456a6d806f73cc
scaelles <scaelles@gmail.com>
2017-09-25 13:04:30 +0200

    Added experiments hiera

--- cluster/wrapper.sh
+++ cluster/wrapper.sh
@@ -3,5 +3,5 @@
 VAL_SEQ_FILE=/srv/glusterfs/kmaninis/Databases/Boundary_Detection/DAVIS/gt_sets/val_categories.txt
 for i in $(cat $VAL_SEQ_FILE)
 	do
-		qsub -N PyOSVOS_$i script.sh $i
+		qsub -N PyOSVOS_$i script.sh $i $1
 	done
