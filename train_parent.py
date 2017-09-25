98b6c9c74a637ae123e93d349f7fbb9586247073
scaelles <scaelles@gmail.com>
2017-09-25 13:43:26 +0200

    Updated master with the result of debugging

--- train_parent.py
+++ train_parent.py
@@ -1,16 +1,30 @@
 # Package Includes
 from __future__ import division
+# import matlab.engine
 import sys
-sys.path.append("/home/eec/Documents/external/deep_learning/pytorch/build/lib.linux-x86_64-2.7")  # Custom PyTorch
 import os
 import socket
 import timeit
+if 'experiments' in os.getcwd():
+    sys.path.append('../../OSVOS-PyTorch')
+else:
+    sys.path.append('OSVOS-PyTorch')
+
+from mypath import Path
+
+if Path.is_custom_pytorch():
+    sys.path.append(Path.custom_pytorch())  # Custom PyTorch
+if Path.is_custom_opencv():
+    sys.path.insert(0, Path.custom_opencv())
 
 # Custom includes
 import visualize as viz
 import osvos_toolbox as tb
 import vgg_osvos as vo
 from custom_layers import class_balanced_cross_entropy_loss
+import numpy as np
+import scipy.misc as sm
+from datetime import datetime
 
 # PyTorch includes
 import torch
@@ -19,73 +33,116 @@ import torch.optim as optim
 from torchvision import transforms, utils
 from torch.utils.data import DataLoader
 
-if 'SGE_GPU' not in os.environ.keys() and socket.gethostname() != 'reinhold':
-    gpu_id = 1  # Select which GPU, -1 if CPU
+# Tensorboard include
+from tensorboardX import SummaryWriter
+
+# Select which GPU, -1 if CPU
+if socket.gethostname() == 'eec':
+    gpu_id = 1
+elif 'SGE_GPU' not in os.environ.keys() and socket.gethostname() != 'reinhold':
+    gpu_id = -1
 else:
     gpu_id = int(os.environ['SGE_GPU'])
+
+print('Using GPU: {} '.format(gpu_id))
 
 # Setting of parameters
 # Parameters in p are used for the name of the model
 p = {
     'trainBatch': 1,  # Number of Images in each mini-batch
-    }
+}
 
 # # Setting other parameters
-nEpochs = 200  # Number of epochs for training
+exp_name = os.path.dirname(os.path.abspath(__file__)).split('/')[-1]
+nEpochs = 240  # Number of epochs for training (500.000/2079)
 useTest = 1  # See evolution of the test set when training?
 testBatch = 1  # Testing Batch
-nTestInterval = 20  # Run on test set every nTestInterval epochs
-db_root_dir = '/media/eec/external/Databases/Segmentation/DAVIS/'
-save_dir = '/home/eec/Desktop/pytorch_experiments/osvos/'
-vis_net = 1  # Visualize the network?
-snapshot = 20  # Store a model every snapshot epochs
+nTestInterval = 5  # Run on test set every nTestInterval epochs
+db_root_dir = Path.db_root_dir()
+save_dir_root = Path.save_root_dir()
+
+if 'experiments' in os.getcwd():
+    save_dir = os.path.join(save_dir_root, 'experiments', exp_name)
+else:
+    save_dir = './models'
+
+if not os.path.exists(save_dir):
+    os.makedirs(os.path.join(save_dir))
+vis_net = 0  # Visualize the network?
+snapshot = 40  # Store a model every snapshot epochs
 nAveGrad = 10
 
+load_caffe_vgg = 1
+resume_epoch = 0  # Default is 0, change if want to resume
 
 # Network definition
-net = vo.OSVOS(pretrained=1)
-if gpu_id >= 0:
-    torch.cuda.set_device(device=gpu_id)
-    net.cuda()
+modelName = exp_name  # tb.construct_name(p, "OSVOS_parent_exact")
+if resume_epoch == 0:
+    if load_caffe_vgg:
+        net = vo.OSVOS(pretrained=2)
+    else:
+        net = vo.OSVOS(pretrained=1)
+else:
+    net = vo.OSVOS(pretrained=0)
+    print("Updating weights from: {}".format(
+        os.path.join(save_dir, modelName + '_epoch-' + str(resume_epoch - 1) + '.pth')))
+    net.load_state_dict(
+        torch.load(os.path.join(save_dir, modelName + '_epoch-' + str(resume_epoch - 1) + '.pth'),
+                   map_location=lambda storage, loc: storage))
+
+# Logging into Tensorboard
+log_dir = os.path.join(save_dir, 'runs', datetime.now().strftime('%b%d_%H-%M-%S') + '_' + socket.gethostname())
+writer = SummaryWriter(log_dir=log_dir, comment='-parent')
+y = net.forward(Variable(torch.randn(1, 3, 480, 854)))
+writer.add_graph(net, y[-1])
 
 # Visualize the network
 if vis_net:
     x = torch.randn(1, 3, 480, 854)
     x = Variable(x)
-    if gpu_id >= 0:
-        x = x.cuda()
     y = net.forward(x)
     g = viz.make_dot(y, net.state_dict())
     g.view()
+
+if gpu_id >= 0:
+    torch.cuda.set_device(device=gpu_id)
+    net.cuda()
 
 # Use the following optimizer
 lr = 1e-8
 wd = 0.0002
 optimizer = optim.SGD([
-    {'params': [pr[1] for pr in net.stages.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd},
-    {'params': [pr[1] for pr in net.stages.named_parameters() if 'bias' in pr[0]], 'lr': lr * 2},
-    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd},
-    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]], 'lr': lr*2},
-    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'weight' in pr[0]], 'lr': lr/10, 'weight_decay': wd},
-    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'bias' in pr[0]], 'lr': 2*lr/10},
-    {'params': net.fuse.weight, 'lr': lr/100, 'weight_decay': wd},
-    {'params': net.fuse.bias, 'lr': 2*lr/100},
-    ], lr=lr, momentum=0.9)
+    {'params': [pr[1] for pr in net.stages.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd,
+     'initial_lr': lr},
+    {'params': [pr[1] for pr in net.stages.named_parameters() if 'bias' in pr[0]], 'lr': 2 * lr, 'initial_lr': 2 * lr},
+    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'weight' in pr[0]], 'weight_decay': wd,
+     'initial_lr': lr},
+    {'params': [pr[1] for pr in net.side_prep.named_parameters() if 'bias' in pr[0]], 'lr': 2 * lr,
+     'initial_lr': 2 * lr},
+    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'weight' in pr[0]], 'lr': lr / 10,
+     'weight_decay': wd, 'initial_lr': lr / 10},
+    {'params': [pr[1] for pr in net.score_dsn.named_parameters() if 'bias' in pr[0]], 'lr': 2 * lr / 10,
+     'initial_lr': 2 * lr / 10},
+    {'params': [pr[1] for pr in net.upscale.named_parameters() if 'weight' in pr[0]], 'lr': 0, 'initial_lr': 0},
+    {'params': [pr[1] for pr in net.upscale_.named_parameters() if 'weight' in pr[0]], 'lr': 0, 'initial_lr': 0},
+    {'params': net.fuse.weight, 'lr': lr / 100, 'initial_lr': lr / 100, 'weight_decay': wd},
+    {'params': net.fuse.bias, 'lr': 2 * lr / 100, 'initial_lr': 2 * lr / 100},
+], lr=lr, momentum=0.9)
 
 
 # Preparation of the data loaders
 # Define augmentation transformations as a composition
 composed_transforms = transforms.Compose([tb.RandomHorizontalFlip(),
                                           tb.Resize(),
-                                        # tb.ScaleNRotate(rots=[0], scales=[0.5, 0.8, 1]),
+                                          # tb.ScaleNRotate(rots=[0], scales=[0.5, 0.8, 1]),
                                           tb.ToTensor()])
 # Training dataset and its iterator
 db_train = tb.DAVISDataset(train=True, inputRes=None, db_root_dir=db_root_dir, transform=composed_transforms)
-trainloader = DataLoader(db_train, batch_size=p['trainBatch'], shuffle=1, num_workers=1)
+trainloader = DataLoader(db_train, batch_size=p['trainBatch'], shuffle=True, num_workers=2)
 
 # Testing dataset and its iterator
 db_test = tb.DAVISDataset(train=False, db_root_dir=db_root_dir, transform=tb.ToTensor())
-testloader = DataLoader(db_test, batch_size=testBatch, shuffle=False, num_workers=1)
+testloader = DataLoader(db_test, batch_size=testBatch, shuffle=False, num_workers=2)
 
 num_img_tr = len(trainloader)
 num_img_ts = len(testloader)
@@ -95,12 +152,9 @@ loss_tr = []
 loss_ts = []
 aveGrad = 0
 
-modelName = tb.construct_name(p, "OSVOS_parent_exact")
-
 print("Training Network")
-
 # Main Training and Testing Loop
-for epoch in range(0, nEpochs):
+for epoch in range(resume_epoch, nEpochs):
     start_time = timeit.default_timer()
     # One training epoch
     for ii, sample_batched in enumerate(trainloader):
@@ -115,18 +169,18 @@ for epoch in range(0, nEpochs):
         outputs = net.forward(inputs)
 
         # Compute the losses, side outputs and fuse
-        losses = [None] * len(outputs)
+        losses = [0] * len(outputs)
         for i in range(0, len(outputs)):
             losses[i] = class_balanced_cross_entropy_loss(outputs[i], gts, size_average=False)
             running_loss_tr[i] += losses[i].data[0]
         loss = (1 - epoch / nEpochs)*sum(losses[:-1]) + losses[-1]
 
         # Print stuff
-        if ii % num_img_tr == num_img_tr-1:
+        if ii % num_img_tr == num_img_tr - 1:
             running_loss_tr = [x / num_img_tr for x in running_loss_tr]
             loss_tr.append(running_loss_tr[-1])
-
-            print('[Epoch: %d, numImages: %5d]' % (epoch+1, ii + 1))
+            writer.add_scalar('data/total_loss_epoch', running_loss_tr[-1], epoch)
+            print('[Epoch: %d, numImages: %5d]' % (epoch, ii + 1))
             for l in range(0, len(running_loss_tr)):
                 print('Loss %d: %f' % (l, running_loss_tr[l]))
                 running_loss_tr[l] = 0
@@ -141,16 +195,17 @@ for epoch in range(0, nEpochs):
 
         # Update the weights once in nAveGrad forward passes
         if aveGrad % nAveGrad == 0:
+            writer.add_scalar('data/total_loss_iter', loss.data[0], ii + num_img_tr * epoch)
             optimizer.step()
             optimizer.zero_grad()
             aveGrad = 0
 
     # Save the model
     if (epoch % snapshot) == snapshot - 1 and epoch != 0:
-        torch.save(net.state_dict(), os.path.join(save_dir, modelName+'_epoch-'+str(epoch+1)+'.pth'))
+        torch.save(net.state_dict(), os.path.join(save_dir, modelName + '_epoch-' + str(epoch) + '.pth'))
 
     # One testing epoch
-    if useTest and epoch % nTestInterval == (nTestInterval-1):
+    if useTest and epoch % nTestInterval == (nTestInterval - 1):
         for ii, sample_batched in enumerate(testloader):
             inputs, gts = sample_batched['image'], sample_batched['gt']
 
@@ -162,18 +217,67 @@ for epoch in range(0, nEpochs):
             outputs = net.forward(inputs)
 
             # Compute the losses, side outputs and fuse
-            losses = [None] * len(outputs)
+            losses = [0] * len(outputs)
             for i in range(0, len(outputs)):
                 losses[i] = class_balanced_cross_entropy_loss(outputs[i], gts, size_average=False)
                 running_loss_ts[i] += losses[i].data[0]
             loss = (1 - epoch / nEpochs) * sum(losses[:-1]) + losses[-1]
 
             # Print stuff
-            if ii % num_img_ts == num_img_ts-1:
+            if ii % num_img_ts == num_img_ts - 1:
                 running_loss_ts = [x / num_img_ts for x in running_loss_ts]
                 loss_ts.append(running_loss_ts[-1])
 
-                print('[Epoch: %d, numImages: %5d]' % (epoch + 1, ii + 1))
+                print('[Epoch: %d, numImages: %5d]' % (epoch, ii + 1))
+                writer.add_scalar('data/test_loss_epoch', running_loss_ts[-1], epoch)
                 for l in range(0, len(running_loss_ts)):
                     print('***Testing *** Loss %d: %f' % (l, running_loss_ts[l]))
                     running_loss_ts[l] = 0
+
+writer.close()
+
+# Test parent network
+net = vo.OSVOS(pretrained=0)
+parentModelName = exp_name  # tb.construct_name(p, 'OSVOS_parent_exact')
+net.load_state_dict(torch.load(os.path.join(save_dir, parentModelName + '_epoch-' + str(nEpochs-1) + '.pth'),
+                               map_location=lambda storage, loc: storage))
+with open(os.path.join(Path.db_root_dir(), 'val_seqs.txt'), 'r') as f:
+    seqs = f.readlines()
+seqs = map(lambda seq: seq.strip(), seqs)
+for seq_name in seqs:
+    # Testing dataset and its iterator
+    db_test = tb.DAVISDataset(train=False, db_root_dir=db_root_dir, transform=tb.ToTensor(), seq_name=seq_name)
+    testloader = DataLoader(db_test, batch_size=1, shuffle=False, num_workers=2)
+
+    save_dir_seq = os.path.join(save_dir, parentModelName, seq_name)
+    if not os.path.exists(save_dir_seq):
+        os.makedirs(save_dir_seq)
+
+    print('Testing Network')
+    # Main Testing Loop
+    for ii, sample_batched in enumerate(testloader):
+
+        img, gt, fname = sample_batched['image'], sample_batched['gt'], sample_batched['fname']
+
+        # Forward of the mini-batch
+        inputs, gts = Variable(img, volatile=True), Variable(gt, volatile=True)
+        if gpu_id >= 0:
+            inputs, gts = inputs.cuda(), gts.cuda()
+
+        outputs = net.forward(inputs)
+
+        for jj in range(int(inputs.size()[0])):
+            pred = np.transpose(outputs[-1].cpu().data.numpy()[jj, :, :, :], (1, 2, 0))
+            pred = 1 / (1 + np.exp(-pred))
+            pred = np.squeeze(pred)
+            img_ = np.transpose(img.numpy()[jj, :, :, :], (1, 2, 0))
+            gt_ = np.transpose(gt.numpy()[jj, :, :, :], (1, 2, 0))
+            gt_ = np.squeeze(gt)
+
+            # Save the result, attention to the index jj
+            sm.imsave(os.path.join(save_dir_seq, os.path.basename(fname[jj]) + '.png'), pred)
+# save_dir = os.path.join(Path.save_root_dir(), parentModelName)
+# eng = matlab.engine.start_matlab('-nodesktop -nodisplay -nosplash -nojvm -r '
+#                                  '"cd {};run initialization.m"'.format(Path.matlab_code()))
+# eng.sweep_threshold(save_dir, 'DAVIS', 'val', 200, 0)
+# eng.quit()
--- vgg_osvos.py
+++ vgg_osvos.py
@@ -4,10 +4,14 @@ import torch
 import math
 from copy import deepcopy
 from custom_layers import center_crop
+import torch.nn.modules as modules
+import numpy as np
+import scipy.io
+from mypath import Path
+import os
 
 
 class OSVOS(nn.Module):
-
     def __init__(self, pretrained=1):
         super(OSVOS, self).__init__()
         lay_list = [[64, 64],
@@ -18,29 +22,33 @@ class OSVOS(nn.Module):
         in_channels = [3, 64, 128, 256, 512]
 
         print("Constructing OSVOS architecture..")
-        stages = []
-        side_prep = []
-        score_dsn = []
-        upscale = []
+        stages = modules.ModuleList()
+        side_prep = modules.ModuleList()
+        score_dsn = modules.ModuleList()
+        upscale = modules.ModuleList()
+        upscale_ = modules.ModuleList()
 
-        # Attention, side_prep and score_dsn start from layer 2
+        # Construct the network
         for i in range(0, len(lay_list)):
             # Make the layers of the stages
             stages.append(make_layers_osvos(lay_list[i], in_channels[i]))
 
+            # Attention, side_prep and score_dsn start from layer 2
             if i > 0:
                 # Make the layers of the preparation step
-                side_prep.append(nn.Sequential(*[nn.Conv2d(lay_list[i][-1], 16, kernel_size=3, padding=1),
-                                                 nn.ReLU(inplace=True)]))
+                side_prep.append(nn.Conv2d(lay_list[i][-1], 16, kernel_size=3, padding=1))
 
                 # Make the layers of the score_dsn step
                 score_dsn.append(nn.Conv2d(16, 1, kernel_size=1, padding=0))
-                upscale.append(nn.Upsample(scale_factor=2**i, mode='bilinear'))
+                # upscale.append(nn.Upsample(scale_factor=2 ** i, mode='bilinear'))
+                upscale_.append(nn.ConvTranspose2d(1, 1, kernel_size=2 ** (1 + i), stride=2 ** i, bias=False))
+                upscale.append(nn.ConvTranspose2d(16, 16, kernel_size=2 ** (1 + i), stride=2 ** i, bias=False))
 
         self.upscale = upscale
-        self.stages = ListModule(*stages)
-        self.side_prep = ListModule(*side_prep)
-        self.score_dsn = ListModule(*score_dsn)
+        self.upscale_ = upscale_
+        self.stages = stages
+        self.side_prep = side_prep
+        self.score_dsn = score_dsn
 
         self.fuse = nn.Conv2d(64, 1, kernel_size=1, padding=0)
 
@@ -48,43 +56,47 @@ class OSVOS(nn.Module):
         self._initialize_weights(pretrained)
 
     def forward(self, x):
-        crop_h, crop_w = int(x.size()[2]), int(x.size()[3])
+        crop_h, crop_w = int(x.size()[-2]), int(x.size()[-1])
         x = self.stages[0](x)
 
         side = []
         side_out = []
         for i in range(1, len(self.stages)):
             x = self.stages[i](x)
-            side_temp = self.upscale[i - 1](self.side_prep[i-1](x))
-            side.append(center_crop(side_temp, crop_h, crop_w))
-            side_out.append(center_crop(self.score_dsn[i - 1](side_temp), crop_h, crop_w))
+            side_temp = self.side_prep[i - 1](x)
+            side.append(center_crop(self.upscale[i - 1](side_temp), crop_h, crop_w))
+            side_out.append(center_crop(self.upscale_[i - 1](self.score_dsn[i - 1](side_temp)), crop_h, crop_w))
 
         out = torch.cat(side[:], dim=1)
         out = self.fuse(out)
-        return side_out + [out]
+        side_out.append(out)
+        return side_out
 
     def _initialize_weights(self, pretrained):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, 0.001)  # math.sqrt(2. / n))
+                m.weight.data.normal_(0, 0.001)
                 if m.bias is not None:
                     m.bias.data.zero_()
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
-                n = m.weight.size(1)
                 m.weight.data.normal_(0, 0.01)
                 m.bias.data.zero_()
+            elif isinstance(m, nn.ConvTranspose2d):
+                m.weight.data.zero_()
+                m.weight.data = interp_surgery(m)
 
-        if pretrained:
-            _vgg = VGG(make_layers([64, 64, 'M', 128, 128, 'M', 256, 256, 256,
-                                    'M', 512, 512, 512, 'M', 512, 512, 512, 'M']))
+        if pretrained == 1:
+            print("Loading weights from PyTorch VGG")
+            vgg_structure = [64, 64, 'M', 128, 128, 'M', 256, 256, 256,
+                             'M', 512, 512, 512, 'M', 512, 512, 512, 'M']
+            _vgg = VGG(make_layers(vgg_structure))
 
             # Load the weights from saved model
-            _vgg.load_state_dict(torch.load('models/vgg16-397923af.pth',
-                                 map_location=lambda storage, loc: storage))
+            _vgg.load_state_dict(torch.load(os.path.join(Path.models_dir(), 'vgg16-397923af.pth'),
+                                            map_location=lambda storage, loc: storage))
 
             # Load the weights directly from the web
             # _vgg.load_state_dict(model_zoo.load_url('https://download.pytorch.org/models/vgg16-397923af.pth'))
@@ -97,6 +109,50 @@ class OSVOS(nn.Module):
                         self.stages[i][j].weight = deepcopy(_vgg.features[inds[k]].weight)
                         self.stages[i][j].bias = deepcopy(_vgg.features[inds[k]].bias)
                         k += 1
+        elif pretrained == 2:
+            print("Loading weights from Caffe VGG")
+            # Load weights from Caffe
+            caffe_weights = scipy.io.loadmat(os.path.join(Path.models_dir(),'vgg_hed_caffe.mat'))
+            # Core network
+            caffe_ind = 0
+            for ind, layer in enumerate(self.stages.parameters()):
+                if ind % 2 == 0:
+                    c_w = torch.from_numpy(caffe_weights['weights'][0][caffe_ind].transpose())
+                    assert (layer.data.shape == c_w.shape)
+                    layer.data = c_w
+                else:
+                    c_b = torch.from_numpy(caffe_weights['biases'][0][caffe_ind][:, 0])
+                    assert (layer.data.shape == c_b.shape)
+                    layer.data = c_b
+                    caffe_ind += 1
+
+def upsample_filt(size):
+    factor = (size + 1) // 2
+    if size % 2 == 1:
+        center = factor - 1
+    else:
+        center = factor - 0.5
+    og = np.ogrid[:size, :size]
+    return (1 - abs(og[0] - center) / factor) * \
+           (1 - abs(og[1] - center) / factor)
+
+
+# set parameters s.t. deconvolutional layers compute bilinear interpolation
+# this is for deconvolution without groups
+def interp_surgery(lay):
+        m, k, h, w = lay.weight.data.size()
+        if m != k:
+            print 'input + output channels need to be the same'
+            raise
+        if h != w:
+            print 'filters need to be square'
+            raise
+        filt = upsample_filt(h)
+
+        for i in range(m):
+            lay.weight[i, i, :, :].data.copy_(torch.from_numpy(filt))
+
+        return lay.weight.data
 
 
 def find_conv_layers(_vgg):
@@ -111,43 +167,15 @@ def make_layers_osvos(cfg, in_channels):
     layers = []
     for v in cfg:
         if v == 'M':
-            layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
+            layers.append(nn.MaxPool2d(kernel_size=2, stride=2, ceil_mode=True))
         else:
             conv2d = nn.Conv2d(in_channels, v, kernel_size=3, padding=1)
-            layers += [conv2d, nn.ReLU(inplace=True)]
+            layers.extend([conv2d, nn.ReLU(inplace=True)])
             in_channels = v
     return nn.Sequential(*layers)
 
 
-class ListModule(nn.Module):
-    """ Allows the creation of lists of layers,
-        which is not supported by PyTorch in general.
-        Detailed discussion here: https://discuss.pytorch.org/t/list-of-nn-module-in-a-nn-module/219
-    """
-    def __init__(self, *args):
-        super(ListModule, self).__init__()
-        idx = 0
-        for module in args:
-            self.add_module(str(idx), module)
-            idx += 1
-
-    def __getitem__(self, idx):
-        if idx < 0 or idx >= len(self._modules):
-            raise IndexError('index {} is out of range'.format(idx))
-        it = iter(self._modules.values())
-        for i in range(idx):
-            next(it)
-        return next(it)
-
-    def __iter__(self):
-        return iter(self._modules.values())
-
-    def __len__(self):
-        return len(self._modules)
-
-
 class VGG(nn.Module):
-
     def __init__(self, features, num_classes=1000):
         super(VGG, self).__init__()
         self.features = features
@@ -198,4 +226,3 @@ def make_layers(cfg, batch_norm=False):
                 layers += [conv2d, nn.ReLU(inplace=True)]
             in_channels = v
     return nn.Sequential(*layers)
-
